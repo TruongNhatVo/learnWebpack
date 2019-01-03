ee050725094da57da13662fc5aebd03d9c7fb58a
Vo Nhat Truong <truong-vn@if-vietnam.com>
2019-01-03 16:17:56 +0700

    Initialize webpack project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import { square, diag } from './test01';
-console.log(square(11)); // 121
-console.log(diag(4, 3)); // 5
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-  const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: __dirname + '/dist',
+    publicPath: '/',
     filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
   }
 };
